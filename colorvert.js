838e080a19c34d7725dc891e58f05dda55521c15
James Pederson <james@jpederson.com>
2014-06-01 21:21:31 -0500

    Cleaned up colorvert.js and versioned package.json for npm publishing.

--- colorvert.js
+++ colorvert.js
@@ -1,47 +1,47 @@
 
 
-// CMYK conversions
+// CMYK
 exports.cmyk_to_rgb = require( "./convert/cmyk-to-rgb" );
 exports.cmyk_to_lab = require( "./convert/cmyk-to-lab" );
 
 
-// Hex Conversions
+// Hex
 exports.hex_to_cmyk = require( "./convert/hex-to-cmyk" );
-exports.hex_to_hsl = require( "./convert/hex-to-hsl" );
-exports.hex_to_hsv = require( "./convert/hex-to-hsv" );
-exports.hex_to_lab = require( "./convert/hex-to-lab" );
-exports.hex_to_rgb = require( "./convert/hex-to-rgb" );
-exports.hex_to_xyz = require( "./convert/hex-to-xyz" );
+exports.hex_to_hsl  = require( "./convert/hex-to-hsl" );
+exports.hex_to_hsv  = require( "./convert/hex-to-hsv" );
+exports.hex_to_lab  = require( "./convert/hex-to-lab" );
+exports.hex_to_rgb  = require( "./convert/hex-to-rgb" );
+exports.hex_to_xyz  = require( "./convert/hex-to-xyz" );
 
 
-// HSL Conversions
-exports.hsl_to_hsv = require( "./convert/hsl-to-hsv" );
-exports.hsl_to_rgb = require( "./convert/hsl-to-rgb" );
+// HSL
+exports.hsl_to_hsv  = require( "./convert/hsl-to-hsv" );
+exports.hsl_to_rgb  = require( "./convert/hsl-to-rgb" );
 
 
-// HSL Conversions
+// HSL
 exports.hsv_to_cmyk = require( "./convert/hsv-to-cmyk" );
-exports.hsv_to_hex = require( "./convert/hsv-to-hex" );
-exports.hsv_to_hsl = require( "./convert/hsv-to-hsl" );
-exports.hsv_to_lab = require( "./convert/hsv-to-lab" );
-exports.hsv_to_rgb = require( "./convert/hsv-to-rgb" );
-exports.hsv_to_xyz = require( "./convert/hsv-to-xyz" );
+exports.hsv_to_hex  = require( "./convert/hsv-to-hex" );
+exports.hsv_to_hsl  = require( "./convert/hsv-to-hsl" );
+exports.hsv_to_lab  = require( "./convert/hsv-to-lab" );
+exports.hsv_to_rgb  = require( "./convert/hsv-to-rgb" );
+exports.hsv_to_xyz  = require( "./convert/hsv-to-xyz" );
 
 
-// Lab conversions
+// Lab
 exports.lab_to_cmyk = require( "./convert/lab-to-cmyk" );
-exports.lab_to_hex = require( "./convert/lab-to-hex" );
-exports.lab_to_hsl = require( "./convert/lab-to-hsl" );
-exports.lab_to_hsv = require( "./convert/lab-to-hsv" );
-exports.lab_to_rgb = require( "./convert/lab-to-rgb" );
-exports.lab_to_xyz = require( "./convert/lab-to-xyz" );
+exports.lab_to_hex  = require( "./convert/lab-to-hex" );
+exports.lab_to_hsl  = require( "./convert/lab-to-hsl" );
+exports.lab_to_hsv  = require( "./convert/lab-to-hsv" );
+exports.lab_to_rgb  = require( "./convert/lab-to-rgb" );
+exports.lab_to_xyz  = require( "./convert/lab-to-xyz" );
 
 
-// RGB conversions
+// RGB
 exports.rgb_to_cmyk = require( "./convert/rgb-to-cmyk" );
-exports.rgb_to_hex = require( "./convert/rgb-to-hex" );
-exports.rgb_to_hsl = require( "./convert/rgb-to-hsl" );
-exports.rgb_to_hsv = require( "./convert/rgb-to-hsv" );
-exports.rgb_to_lab = require( "./convert/rgb-to-lab" );
-exports.rgb_to_xyz = require( "./convert/rgb-to-xyz" );
+exports.rgb_to_hex  = require( "./convert/rgb-to-hex" );
+exports.rgb_to_hsl  = require( "./convert/rgb-to-hsl" );
+exports.rgb_to_hsv  = require( "./convert/rgb-to-hsv" );
+exports.rgb_to_lab  = require( "./convert/rgb-to-lab" );
+exports.rgb_to_xyz  = require( "./convert/rgb-to-xyz" );
 
